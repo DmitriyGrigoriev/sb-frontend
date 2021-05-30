8f27f50a67be6f4923a40bfa220404a17589e3a0
Dmitriy Grigorev <dimag@inbox.ru>
2021-05-30 13:46:31 +0300

    style: Change HTML part of component.

--- src/ui/page/PageContainer.vue
+++ src/ui/page/PageContainer.vue
@@ -1,12 +1,17 @@
 <template>
-  <q-page class="q-pt-lg q-pb-md">
-    <div class="row">
+  <q-page>
+    <slot name="title"></slot>
+
+    <!-- <div class="row">
       <div
         class="offset-xl-2 col-xl-8 offset-lg-3 col-lg-6 offset-md-1 col-md-10 offset-sm-1 col-sm-10 offset-xs-0 col-xs-12"
       >
-        <!-- Content goes here -->
         <slot></slot>
       </div>
+    </div> -->
+
+    <div class="q-pa-md">
+      <slot></slot>
     </div>
 
     <!-- QPageScroller -->
