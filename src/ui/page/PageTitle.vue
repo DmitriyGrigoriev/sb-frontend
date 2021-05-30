dbadbdf14e5955942f061fdf2eadf852718e8de3
Dmitriy Grigorev <dimag@inbox.ru>
2021-05-30 13:46:41 +0300

    style: Change HTML part of component.

--- src/ui/page/PageTitle.vue
+++ src/ui/page/PageTitle.vue
@@ -1,18 +1,26 @@
 <template>
-  <q-toolbar>
-    <q-item-label header class="q-pl-none text-h6 text-uppercase">
-      {{ title }}
-    </q-item-label>
+  <div class="q-mb-md">
+    <q-toolbar class="bg-dark text-white">
+      <q-avatar v-if="$route.fullPath !== '/dashboard'">
+        <q-btn icon="chevron_left" flat round dark @click="$router.back()" />
+      </q-avatar>
+      <q-avatar v-if="icon">
+        <q-icon :name="icon"></q-icon>
+      </q-avatar>
+      <!-- will be visible only if screen is greater than sm -->
+      <q-toolbar-title class="gt-sm text-uppercase">{{ title }}</q-toolbar-title>
 
-    <q-space />
+      <q-space />
 
-    <slot></slot>
-  </q-toolbar>
+      <slot></slot>
+    </q-toolbar>
+    <q-separator />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'PageTitle',
-  props: ['title']
+  props: ['title', 'icon']
 }
 </script>
