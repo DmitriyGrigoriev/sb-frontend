4a6758d5b52d25baf222306f278f23293f5786cf
Dmitriy Grigorev <dimag@inbox.ru>
2021-05-30 13:43:29 +0300

    refactor: Add links to new services.

--- src/services/index.js
+++ src/services/index.js
@@ -3,9 +3,11 @@ export * from './user.service'
 export * from './api.service'
 export * from './profile.service'
 export * from './storage.service'
-// export * from './contact.service';
-// export * from './exchange-rate.service';
-// export * from './project.service';
-// export * from './resource.service';
-// export * from './user.service';
-// export * from './version.service';
+// export * from './contact.service'
+// export * from './exchange-rate.service'
+export * from './program.service'
+export * from './project.service'
+export * from './dictionary.service'
+// export * from './resource.service'
+// export * from './user.service'
+// export * from './version.service'
