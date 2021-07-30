27a6dae8d0f219b28127d7381bcfc5fb47f6ec74
Dmitriy Grigorev <dimag@inbox.ru>
2021-07-30 13:27:34 +0300

    refactor: Some methods moved to DataTable.js

--- src/modules/unitofmeasure/pages/MeasureIndex.vue
+++ src/modules/unitofmeasure/pages/MeasureIndex.vue
@@ -1,49 +1,35 @@
 <template>
   <page-container>
     <template v-slot:title>
-      <page-title :title="$t('pages.dictionary.title')" icon="fas fa-snowflake"></page-title>
+      <page-title
+        :title="$t('pages.dictionary.title')"
+        icon="fas fa-snowflake"
+      >
+      </page-title>
     </template>
+
     <div class="q-pa-md">
-      <q-table
+      <data-table
         :data="serverData"
         :columns="columns"
         :loading="loading"
         :pagination.sync="serverPagination"
-        :filter="filter"
-        row-key="code"
         @request="requestUnitOfMeasure"
-        @row-click="rowClicked"
-        wrap-cells
-        separator="cell"
+        @rowclick="rowClicked"
+        @addrow="addRow"
+        @deleterows="deleteRows"
         :selected.sync="selected"
         selection="multiple"
-        :selected-rows-label="getSelectedString"
-        :title="$t('pages.dictionary.measure.title')"
-        :no-data-label="$t('table.props.no_data_available')"
-        :rows-per-page-label="$t('table.props.rows_per_page')"
-        :no-results-label="$t('table.props.no_filter_result')"
-        style="margin-bottom: 70px;"
       >
-        <template v-slot:header-cell="props">
-          <q-th :props="props">
-            <strong>{{ props.col.label }}</strong>
-          </q-th>
+<!--        :title="$t('pages.dictionary.measure.title')"-->
+        <template v-slot:top-left>
+          <dictionary-select
+            ref="selectDictionary"
+          >
+          </dictionary-select>
         </template>
 
-        <template v-slot:top-right="props">
-          <search v-model="filter"></search>
-
-          <icon-button icon="add" :tooltip="$t('buttons.addrow.tooltip')" @click="addUnitRow" />
-
-          <icon-button icon="delete" color="red" :disabled="mayDelete" :tooltip="$t('buttons.deleterow.tooltip')" @click="deleteUnitRow" />
-
-          <icon-button icon="refresh" :tooltip="$t('pages.dictionary.measure.buttons.refresh.tooltip')" @click="refetch" />
-
-          <fullscreen-button @click="props.toggleFullscreen" :in-fullscreen="props.inFullscreen"></fullscreen-button>
-
-        </template>
-
-      </q-table>
+      </data-table>
     </div>
 
   </page-container>
@@ -51,39 +37,25 @@
 
 <script>
 /* eslint-disable camelcase */
-import { mapGetters, mapActions } from 'vuex'
-import {
-  FullscreenButton,
-  Search
-} from '@/ui'
+import { mapActions } from 'vuex'
 import PageContainer from '@/ui/page/PageContainer.vue'
 import PageTitle from '@/ui/page/PageTitle.vue'
-import IconButton from '@/ui/buttons/IconButton'
-// import HelpButton from '@/ui/buttons/HelpButton'
-// import { openURL } from 'quasar'
+import DataTable from '../components/DataTable'
+import DictionarySelect from '../components/DictionarySelect'
 import { handleError, handleResponse } from '@/utils'
+import DataTableMixin from '../mixins/DataTable'
 
 export default {
   name: 'UnitOfMeasureIndex',
+  mixins: [DataTableMixin],
   components: {
-    // HelpButton,
-    IconButton,
-    Search,
-    FullscreenButton,
+    DataTable,
+    DictionarySelect,
     PageContainer,
     PageTitle
   },
-  computed: {
-    ...mapGetters('auth', ['isEncoder']),
-    mayDelete () {
-      return this.selected.length === 0
-    }
-  },
   data () {
     return {
-      filter: '',
-      loading: false,
-      selected: [],
       columns: [
         {
           name: 'code',
@@ -106,31 +78,22 @@ export default {
           align: 'left',
           sortable: true
         }
-      ],
-      serverData: [],
-      serverPagination: {
-        page: 1,
-        rowsNumber: 0, // specifying this determines pagination is server-side
-        rowsPerPage: 5, // current rows per page being displayed
-        descending: true
-      }
+      ]
     }
+  },
+  computed: {
   },
   methods: {
     ...mapActions('measure', ['fetchMeasure', 'deleteMeasure']),
     refetch () {
-      this.requestUnitOfMeasure({
-        pagination: this.serverPagination
-      })
+      this.requestUnitOfMeasure(this.defaultPagination)
     },
     async requestUnitOfMeasure (props) {
       this.loading = true
       this.serverPagination = props.pagination
-      const query = {
-        page: props.pagination.page,
-        rows_per_page: props.pagination.rowsPerPage,
-        search: this.filter
-      }
+
+      const query = this.defaultQuery(props)
+
       const response = await this.fetchMeasure(query)
       setTimeout(() => {
         const data = handleResponse(response)
@@ -139,42 +102,15 @@ export default {
         this.loading = false
       }, 500)
     },
-    addUnitRow () {
-      this.$router.push({ name: 'add-measure' })
-    },
-    rowClicked (evt, row) {
-      this.$router.push({ path: `edit-measure/${row.id}` })
-    },
-    deleteUnitRow () {
-      this.$q
-        .dialog({
-          title: this.$t('pages.dictionary.measure.dialogues.confirm_delete.title'),
-          message: this.$t('pages.dictionary.measure.dialogues.confirm_delete.message'),
-          cancel: {
-            label: this.$t('pages.dictionary.measure.dialogues.confirm_delete.buttons.cancel')
-          },
-          ok: {
-            label: this.$t('pages.dictionary.measure.dialogues.confirm_delete.buttons.ok')
-          }
+    deleteRows () {
+      this.deleteMeasure(this.selected)
+        .then(() => {
+          this.refetch()
         })
-        .onOk(() => {
-          this.$q.loading.show()
-          this.deleteMeasure(this.selected)
-            .then(() => {
-              this.clearSelected()
-              this.refetch()
-            })
-            .catch(error => {
-              handleError(error)
-            })
-            .finally(() => this.$q.loading.hide())
+        .catch(error => {
+          handleError(error)
         })
-    },
-    clearSelected () {
-      this.selected = []
-    },
-    getSelectedString () {
-      return this.selected.length === 0 ? '' : `${this.selected.length} record${this.selected.length > 1 ? 's' : ''} selected of ${this.serverData.length}`
+        .finally(() => this.$q.loading.hide())
     },
     showHelp () {
       const content = this.$t('pages.dictionary.measure.help')
@@ -186,12 +122,9 @@ export default {
       })
     }
   },
-  created () {
-    this.refetch()
-  }
+  created () {}
 }
 </script>
 
 <style scoped>
-
 </style>
