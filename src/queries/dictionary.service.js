aa1f5a57b6be788395a37a682786e6605ab94e4d
Dmitriy Grigorev <dimag@inbox.ru>
2021-07-30 13:54:13 +0300

    feat: Added some new methods.

--- src/queries/dictionary.service.js
+++ src/queries/dictionary.service.js
@@ -7,12 +7,23 @@ export const MEASURE = {
 export function GET_MEASURE (query) {
   return {
     method: 'get',
-    url: '/api/v1/unitofmeasure/',
+    url: MEASURE.url,
     params: query
   }
 }
 export function GET_MEASURE_RECORD (id) {
   return {
-    url: `/api/v1/unitofmeasure/${id}/`
+    url: `${MEASURE.url}${id}/`
+  }
+}
+// «VAT Posting Group» («НДС  Учетная Группа»)
+export const VAT = {
+  url: '/api/v1/vat/'
+}
+export function GET_VAT (query) {
+  return {
+    method: 'get',
+    url: VAT.url,
+    params: query
   }
 }
