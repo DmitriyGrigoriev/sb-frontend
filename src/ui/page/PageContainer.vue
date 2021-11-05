f7ea57eb74ff442d9a9a150637934ffb0753e517
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:31:14 +0300

    style: Remove unused comments

--- src/ui/page/PageContainer.vue
+++ src/ui/page/PageContainer.vue
@@ -2,14 +2,6 @@
   <q-page>
     <slot name="title"></slot>
 
-    <!-- <div class="row">
-      <div
-        class="offset-xl-2 col-xl-8 offset-lg-3 col-lg-6 offset-md-1 col-md-10 offset-sm-1 col-sm-10 offset-xs-0 col-xs-12"
-      >
-        <slot></slot>
-      </div>
-    </div> -->
-
     <div class="q-pa-md">
       <slot></slot>
     </div>
