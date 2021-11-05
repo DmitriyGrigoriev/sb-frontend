1974958b5a320853aeaeed5c1d5b62e3ad46bb2e
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:17:47 +0300

    chore: Redesign 401 interceptor

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,14 +1,45 @@
 import axios from 'axios'
 // import { handleError } from '@/utils'
 import { storageService } from './storage.service'
-import { REFRESH_TOKEN } from '@/queries'
+import { authQueries } from '@/queries'
+
 import store from '@/store'
+import loading from '@/functions/loading'
+
+// class ProtectedError extends Error {
+//   constructor (errorCode, message) {
+//     super(message)
+//     this.name = this.constructor.name
+//     this.message = message
+//     this.errorCode = errorCode
+//   }
+// }
 
 axios.defaults.baseURL = process.env.PROD_ENDPOINT
 axios.defaults.timeout = 20000 // request timeout
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common.Accept = 'application/json'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
+
+axios.interceptors.request.use(config => {
+  // Show loading cursor, use loading: 'gear' or 'hourglass' in your queries
+  loading.show(config)
+  return config
+}, error => {
+  // Do something with request error
+  Promise.reject(error)
+})
+/*
+// axios.interceptors.response.use(response => {
+//   // Hide loading
+//   loading.hide(response.config)
+//   return response
+// }, error => {
+//   loading.hide(error.config)
+//   // Do something with response error
+//   Promise.reject(error)
+// })
+*/
 
 const apiService = {
   // Stores the 401 interceptor position so that it can be later ejected when needed
@@ -56,13 +87,23 @@ const apiService = {
   mount401Interceptor () {
     this._401interceptor = axios.interceptors.response.use(
       (response) => {
+        if (Object.prototype.hasOwnProperty.call(response, 'config')) {
+          loading.hide(response.config)
+        }
         return response
       },
       async (error) => {
+        const query = authQueries.refreshToken()
+        // if (Object.prototype.hasOwnProperty.call(error, 'request') && error.request.status === 550) {
+        //   throw new ProtectedError(
+        //     error.response.status,
+        //     error.response.data.detail
+        //   )
+        // }
         if (Object.prototype.hasOwnProperty.call(error, 'request') && error.request.status === 401) {
-          if (error.config.url.includes(REFRESH_TOKEN.url)) {
+          if (error.config.url.includes(query.url)) {
             // Refresh token has failed. Logout the user
-            await this.$store.dispatch('auth/signoutUser')
+            await this.$store.dispatch('auth/clearStore')
             throw error
           } else {
             // Refresh the access token
@@ -83,6 +124,12 @@ const apiService = {
         }
         // handleError(error)
         // If error was not 401 just reject as is
+        // if (error.response.status === 550) {
+        //   throw new ProtectedError(
+        //     error.response.status,
+        //     error.response.data.detail
+        //   )
+        // } else { throw error }
         throw error
       }
     )
