008f5485904aaff7fe0218149ee24b1adcdd7bf2
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:26:41 +0300

    chore: Change allow list of menu

--- src/ui/components/Menu.vue
+++ src/ui/components/Menu.vue
@@ -1,27 +1,27 @@
 <template id="menu">
   <q-list dense>
-    <menu-item :label="$t('menus.dashboard.label')" @click="selectElement({ name: 'dashboard' })" icon="fas fa-tachometer-alt" />
-    <menu-item label="Programs" @click="selectElement({ name: 'programs' })" icon="fas fa-list" v-if="!isReviewer" />
-    <menu-item label="Projects" @click="selectElement({ name: 'projects' })" icon="fas fa-tasks" v-if="!isReviewer" />
-    <menu-item label="Review Programs" @click="selectElement({ name: 'review' })" icon="fas fa-list" v-if="isReviewer" />
-    <menu-item label="Review Projects" @click="selectElement({name: 'projects' })" icon="fas fa-tasks" v-if="isReviewer" />
-    <menu-item label="Consolidators" @click="selectElement({ name: 'consolidates' })" icon="fas fa-layer-group" />
-    <menu-item label="Deleted Projects" @click="selectElement({ name: 'deleted-projects' })" icon="fas fa-trash-alt" />
+    <menu-item :label="$t('shortcuts.dashboard.label')" @click="selectElement({ name: 'dashboard' })" icon="fas fa-tachometer-alt" />
+<!--    <menu-item label="Programs" @click="selectElement({ name: 'programs' })" icon="fas fa-list" v-if="!isReviewer" />-->
+<!--    <menu-item label="Projects" @click="selectElement({ name: 'projects' })" icon="fas fa-tasks" v-if="!isReviewer" />-->
+<!--    <menu-item label="Review Programs" @click="selectElement({ name: 'review' })" icon="fas fa-list" v-if="isReviewer" />-->
+<!--    <menu-item label="Review Projects" @click="selectElement({name: 'projects' })" icon="fas fa-tasks" v-if="isReviewer" />-->
+    <menu-item :label="$t('shortcuts.service.label')" @click="selectElement({ name: 'service' })" icon="fas fa-layer-group" />
+<!--    <menu-item label="Deleted Projects" @click="selectElement({ name: 'deleted-projects' })" icon="fas fa-trash-alt" />-->
     <q-separator />
-    <menu-item :label="$t('menus.profile.label')" @click="selectElement({ name: 'profile' })" icon="fas fa-id-card" />
-    <menu-item :label="$t('menus.admin.label')" @click="selectElement({ name: 'admin' })" icon="fas fa-user-shield" />
-    <menu-item :label="$t('menus.security.label')" @click="selectElement({ name: 'security' })" icon="fas fa-fingerprint" />
-    <menu-item :label="$t('menus.settings.label')" @click="selectElement({ name: 'settings' })" icon="fas fa-cog" />
+    <menu-item :label="$t('shortcuts.profile.label')" @click="selectElement({ name: 'profile' })" icon="fas fa-id-card" />
+    <menu-item :label="$t('shortcuts.admin.label')" @click="selectElement({ name: 'admin' })" icon="fas fa-user-shield" />
+    <menu-item :label="$t('shortcuts.security.label')" @click="selectElement({ name: 'security' })" icon="fas fa-fingerprint" />
+    <menu-item :label="$t('shortcuts.settings.label')" @click="selectElement({ name: 'settings' })" icon="fas fa-cog" />
     <q-separator />
     <menu-item
-      :label="$t('menus.askhelp.label')"
+      :label="$t('shortcuts.askhelp.label')"
       icon="fab fa-slack"
-      @click="openURL($t('menus.askhelp.url'))"
+      @click="openURL($t('shortcuts.askhelp.url'))"
       type="a"
       target="_blank"
     />
     <q-separator></q-separator>
-    <menu-item :label="$t('menus.logout.label')" icon="exit_to_app" @click="handleSignoutUser"></menu-item>
+    <menu-item :label="$t('shortcuts.logout.label')" icon="exit_to_app" @click="handleSignoutUser"></menu-item>
   </q-list>
 </template>
 
@@ -39,7 +39,7 @@ export default {
   },
   methods: {
     selectElement (name) {
-      this.$router.push(name)
+      this.pushRoute(name)
     },
     handleSignoutUser () {
       this.$q
@@ -50,7 +50,53 @@ export default {
         })
         .onOk(() => this.$store.dispatch('auth/signoutUser'))
     },
-    openURL
+    openURL,
+    // Compare the $route.matched against the desired route
+    // https://stackoverflow.com/questions/62462276/how-to-solve-avoided-redundant-navigation-to-current-location-error-in-vue
+    pushRoute (route) {
+      // if sending path:
+      if (typeof (route) === 'string') {
+        if (this.$route.path !== route) {
+          this.$router.push(route)
+        }
+      } else { // if sending a {name: '', ...}
+        if (this.$route.name === route.name) {
+          if ('params' in route) {
+            let routesMatched = true
+            for (const key in this.$route.params) {
+              const value = this.$route.params[key]
+              if (value === null || value === undefined) {
+                if (key in route.params) {
+                  if (route.params[key] !== undefined && route.params[key] !== null) {
+                    routesMatched = false
+                    break
+                  }
+                }
+              } else {
+                if (key in route.params) {
+                  if (this.$route.params[key] !== value) {
+                    routesMatched = false
+                    break
+                  }
+                } else {
+                  routesMatched = false
+                  break
+                }
+              }
+              if (!routesMatched) {
+                this.$router.push(route)
+              }
+            }
+          } else {
+            if (Object.keys(this.$route.params).length !== 0) {
+              this.$router.push(route)
+            }
+          }
+        } else {
+          this.$router.push(route)
+        }
+      }
+    }
   }
 }
 </script>
