2d220e7f3bb433aded7754eb8dd0b8413a4e6389
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:33:15 +0300

    feat: Add new HandleErrorClass

--- src/utils/handle-error.js
+++ src/utils/handle-error.js
@@ -1,21 +1,200 @@
 import { translate } from '@/utils/translate'
 import store from '@/store'
 import Router from '@/router'
+// import { EventBus } from '@/functions'
 import {
   showErrorNotification
 } from '@/functions/function-show-notifications'
 import {
-  showRestQLErrorMessage
+  showRestQLErrorMessage,
+  showCustomErrorMessage
 } from '@/functions/function-rest-error-messages'
 
 // Attempt to define `$t` in this context
 const $t = translate
 
-export const handleError = error => {
-  handleError.message(error, true)
-  return Promise.reject(error)
+// Custom errors
+// Todo: Write code to send error into outside back tracking system
+export class CustomError extends Error {
+  constructor (errorCode, message) {
+    super(message)
+    // this.name = this.constructor.name
+    const title = $t('messages.event.custom_error')
+    this.message = `${title} ${message}`
+    this.customErrorCode = errorCode
+  }
+
+  printError () {
+    console.log(this.stack) // MyError: message \n
+  }
 }
-handleError.message = function (error, showErrorMessage) {
+
+export class HandleErrorClass {
+  message = $t('responses.errors.network_error.message')
+  store = null
+  error = null
+  status = null
+
+  constructor (error) {
+    if (typeof error.response !== 'undefined') {
+      this.status = this.error.response.status
+    } else {
+      if (error.message !== 'Network Error') {
+        this.error = new CustomError(5000, error.message)
+      }
+    }
+    this.store = store
+    this.error = error
+    this.message = this.errorMessage()
+  }
+
+  _hasOwnProperty (obj, property) {
+    return Object.prototype.hasOwnProperty.call(obj, property)
+  }
+
+  determineTypeError () {
+    // Avoid a redundant navigation
+    if (this._hasOwnProperty(this.error, 'name') && this.error.name === 'NavigationDuplicated') {
+      return Promise.resolve()
+    }
+    switch (this.status) {
+      case 401:
+        if (this._hasOwnProperty(this.error.response.data, 'code')) {
+          if (this.error.response.data.code === 'token_not_valid') {
+            this.invalidToken()
+          }
+        } else {
+          /* invalid credentials: login, password or unactivated email */
+          this.invalidCredential()
+        }
+        break
+      case 403:
+        return Promise.resolve()
+      case 404:
+        return Promise.resolve()
+      case 405:
+        return Promise.resolve()
+      case 409:
+        return Promise.resolve()
+      case 5000:
+        this.error.printError()
+        return Promise.resolve()
+      default:
+        this.networkError()
+        break
+    }
+  }
+
+  errorMessage () {
+    switch (this.status) {
+      case 401:
+        if (this._hasOwnProperty(this.error.response.data, 'code')) {
+        } else {
+          this.message = $t('auth.login.invalid_credentials')
+        }
+        break
+      case 403:
+        if (this._hasOwnProperty.call(this.error.response.data, 'detail')) {
+          this.message = this.error.response.data.detail
+        } else {
+          this.message = $t('auth.login.verification_required')
+        }
+        showErrorNotification(this.message)
+        break
+      case 404:
+        this.message = $t('messages.event.error404')
+        break
+      case 405:
+        this.message = 'API Route Method Not Allowed'
+        showErrorNotification(this.message)
+        break
+      case 409:
+        this.message = $t('responses.errors.conflict.message')
+        break
+      case 500:
+        this.message = $t('responses.errors.internal_server_error.message')
+        break
+      case 550:
+        this.message = 'Protected Error'
+        break
+      case 5000:
+        this.message = this.error.message
+        showCustomErrorMessage(this.error)
+        break
+      default:
+        if (this.error.response.status >= 500) {
+          this.message = $t('responses.errors.internal_server_error.message')
+        } else {
+          this.message = $t('responses.errors.network_error.message')
+        }
+        break
+    }
+    return this.message
+  }
+
+  invalidToken () {
+    const Store = this.store
+    return new Promise((resolve, reject) => {
+      Store.dispatch('auth/clearStore')
+      Router.push({ name: 'home' })
+        .then(() => {
+          resolve()
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  }
+
+  invalidCredential () {
+    /* invalid credentials: login, password or unactivated email */
+    Router.push({ name: 'login' }).catch(() => {
+      // Avoid it redundant route error
+    })
+  }
+
+  networkError () {
+    // this.store.dispatch('auth/signoutUser')
+    const Store = this.store
+    return new Promise((resolve, reject) => {
+      Store.dispatch('auth/signoutUser')
+        .then(() => {
+          resolve()
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  }
+}
+
+export const handleError = error => {
+  const handle = new HandleErrorClass(error)
+  return new Promise(function () {
+    handle.determineTypeError()
+  })
+    .then(() => {
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  // return new Promise((resolve, reject) => {
+  //   handle.determineTypeError()
+  //     .then((val) => {
+  //       resolve(val)
+  //     })
+  //     .catch((err) => {
+  //       reject(err)
+  //     })
+  // })
+}
+
+// export const handleError = error => {
+//   // handleError.message(error, true)
+//   return Promise.resolve(alert(error.message))
+// }
+
+handleError.message = function (error, showError) {
   // Setup Error Message
   let message = $t('responses.errors.network_error.message')
   if (error.message === 'Network Error') {
@@ -28,7 +207,19 @@ handleError.message = function (error, showErrorMessage) {
     if (error.response.status === 401) {
       if (Object.prototype.hasOwnProperty.call(error.response.data, 'code')) {
         if (error.response.data.code === 'token_not_valid') {
-          store.dispatch('auth/signoutUser')
+          store.dispatch('auth/clearStore')
+          // store.dispatch('auth/signoutUser')
+          // const asyncfn = () => {
+          //   return new Promise((resolve, reject) => {
+          //     setTimeout(() => {
+          //       resolve(Router.push({ name: 'home' }))
+          //     }, 1000)
+          //   })
+          // }
+          // const routePush = async () => {
+          //   await asyncfn()
+          // }
+          // routePush()
           setTimeout(() => {
             Router.push({ name: 'home' })
           }, 1000)
@@ -36,23 +227,30 @@ handleError.message = function (error, showErrorMessage) {
       } else {
         /* invalid credentials: login, password or unactivated email */
         message = $t('auth.login.invalid_credentials')
-        setTimeout(() => {
-          Router.push({ name: 'login' })
-        }, 1000)
+        Router.push({ name: 'login' })
+        // setTimeout(() => {
+        //   Router.push({ name: 'login' })
+        // }, 1000)
       }
     } else if (error.response.status === 403) {
       if (Object.prototype.hasOwnProperty.call(error.response.data, 'detail')) {
         message = error.response.data.detail
       } else { message = $t('auth.login.verification_required') }
     } else if (error.response.status === 404) {
-      message = 'API Route is Missing or Undefined'
+      return Router.replace({ name: 'error' })
+      // message = 'API Route is Missing or Undefined'
     } else if (error.response.status === 405) {
       message = 'API Route Method Not Allowed'
+    } else if (error.response.status === 409) {
+      message = $t('responses.errors.conflict.message')
     } else if (error.response.status === 400) {
       // Validation Message
       showRestQLErrorMessage(error)
-      showErrorMessage = null
+      showError = null
       // message = 'В запросе к серверу используется неверный параметр или его значение.'
+    } else if (error.response.status === 550) {
+      // showError = null
+      // return EventBus.$emit('protected-error', error)
     } else if (error.response.status >= 500) {
       message = $t('responses.errors.internal_server_error.message')
     }
@@ -64,9 +262,15 @@ handleError.message = function (error, showErrorMessage) {
         message = error.response.data.message
       }
     }
+  } else {
+    if (error.message !== 'Network Error') {
+      const customError = new CustomError(5000, error.message)
+      customError.printError()
+      return showCustomErrorMessage(customError)
+    }
   }
-  if (showErrorMessage) {
-    showErrorNotification({ message: message })
+  if (showError) {
+    showErrorNotification(message)
   }
   return message
 }
