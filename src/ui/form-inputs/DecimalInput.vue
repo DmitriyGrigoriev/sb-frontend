908e5bcad81f33e162505ff71f7b15ab93a2c497
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:27:32 +0300

    feat: Initial commit

---
+++ src/ui/form-inputs/DecimalInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col">
+    <span v-if="labeled" class="text-caption text-weight-bold">{{ label }}</span>
+    <q-input
+      v-model="model"
+      :dense="dense"
+      :readonly="readonly"
+      :rules="rules"
+      :hint="hint"
+      :clearable="clearable"
+      labeled
+      type="number"
+      mask="#.##"
+      fill-mask="#"
+      :outlined="outlined"
+      label-color="orange-10"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'DecimalInput',
+  props:
+    {
+      label: String,
+      labeled: {
+        type: Boolean,
+        default: true
+      },
+      outlined: {
+        type: Boolean,
+        default: true
+      },
+      clearable: {
+        type: Boolean,
+        default: true
+      },
+      value: [String, Number],
+      readonly: Boolean,
+      rules: Array,
+      hint: String
+    },
+  computed: {
+    ...mapState('settings', ['dense']),
+    model: {
+      get () {
+        return this.$props.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
