84714f391792cc3ab7fbb0d527908c25decd11d1
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-05 18:14:44 +0300

    fix: Move functions to class

--- src/queries/user.service.js
+++ src/queries/user.service.js
@@ -1,4 +1,41 @@
 /* eslint-disable camelcase */
+const userQueries = {
+  // Get list of users
+  getUsers () {
+    return {
+      method: 'get',
+      url: '/auth/users/'
+      // loading: 'gears'
+    }
+  },
+  // Get user info
+  getUser (user_id) {
+    return {
+      method: 'get',
+      url: `/auth/users/${user_id}/`
+    }
+  },
+  // Get list roles
+  getRoles () {
+    return {
+      method: 'get',
+      url: '/auth/roles/'
+    }
+  },
+  // Get list of user's roles
+  getUserRoles (user_id, role_ids) {
+    return {
+      method: 'put',
+      url: `/auth/user-role/${user_id}/`,
+      data: {
+        id: user_id,
+        role: role_ids
+      }
+    }
+  }
+}
+
+export { userQueries }
 
 export const GET_USERS =
   {
