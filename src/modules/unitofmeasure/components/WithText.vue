148a2d4d01ea09507fa64b4c2c7ef166227ec2b2
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-30 16:03:26 +0300

    fix: Add WithText component

---
+++ src/modules/unitofmeasure/components/WithText.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="column q-pa-sm">
+    <div class="row text-caption text-weight-bold items-center justify-between">
+      {{ label }}
+    </div>
+    <div class="row text-body2">
+      <div class="col">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WithText',
+  props: ['label']
+}
+</script>
--- src/ui/components/LabelValue.vue
+++ src/ui/components/LabelValue.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import TextareaCopy from 'TextareaCopy'
+import TextareaCopy from './TextareaCopy'
 
 export default {
   name: 'LabelValue',
